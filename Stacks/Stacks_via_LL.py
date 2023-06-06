8aa3cf835f3ff646895ccda5aae8e649cad70fcf
Parth Madaan <122528926+parthdacoder@users.noreply.github.com>
2023-06-07 04:23:18 +0530

    Add files via upload

---
+++ Stacks/Stacks_via_LL.py
@@ -0,0 +1,39 @@
+# Stack implementation via Linked Lists
+
+class Node:
+
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+
+class Stack:
+
+    def __init__(self):
+        self.top = None
+
+    def isEmpty(self):
+        return self.top == None
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    def traverse(self):
+        temp = self.top
+        while temp != None:
+            print(temp.data)
+            temp = temp.next
+
+    def peek(self):
+        if (self.isEmpty()):
+            return "Stack Empty"
+        else:
+            return self.top.data
+
+    def pop(self):
+        if (self.isEmpty()):
+            return "Stack Empty"
+        else:
+            self.top = self.top.next
