731a310cefb6e3031fb259d896aae11babe8af99
Parth Madaan <122528926+parthdacoder@users.noreply.github.com>
2023-06-19 07:40:05 +0530

    Add files via upload

---
+++ Graphs/AdjacenyList.py
@@ -0,0 +1,21 @@
+class GraphNode:
+    def __init__(self, val):
+        self.val = val
+        self.neighbors = []
+        
+adjList = { "A": [], "B": []}
+
+edges = [["A","B"], ["B","C"], ["B","E"], ["C","E"], ["E","D"]]
+
+adjList = {}
+
+for src, dst in edges:
+    if src not in adjList:
+        adjList[src] = []
+    if dst not in adjList:
+        adjList[dst] = []
+        
+    adjList[src].append(dst)
+    
+for node, neighbors in adjList.items():
+    print(f"Node {node} has neighbors: {neighbors}")
