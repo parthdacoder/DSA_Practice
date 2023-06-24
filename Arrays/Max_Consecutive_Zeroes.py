2df4c06c83cb14577caf02bfa4d41c969b896143
Parth Madaan <122528926+parthdacoder@users.noreply.github.com>
2023-06-25 00:42:09 +0530

    Update Max_Consecutive_Zeroes.py

--- Arrays/Max_Consecutive_Zeroes.py
+++ Arrays/Max_Consecutive_Zeroes.py
@@ -1,17 +1,18 @@
 
-
 def getMaxLength(arr):
-    num1 = 0
-    num2 = 0
-    for i in arr:
-        if i == 0:
-            num1 += 1
-            
-        if i != 0:
-            
+    count = 0
+    result = 0
+    for i in range(0,len(arr)):
+        if arr[i] != 0:
+            count = 0
+        
+        else: 
+            count += 1
+            result  = max(count, result)
+    return result
 
-    return num  
+  
     
-arr = [0,0,1,2,3,4,1,0]
+arr = [0,0,1,2,3,4,1,0,0,0]
 
 print(getMaxLength(arr))
